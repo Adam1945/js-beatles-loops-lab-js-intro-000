ca95cf1ea51c0e132cbb1355d0c677c4e6fa619f
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 20:07:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
-  for(var i= 0; i<=musicians.length; i++) {
+  for(var i = 0; i <= musicians.length-1; i++) {
     var string = `${musicians[i]} plays ${instruments[i]}`;
     arr.push(string);
   }
